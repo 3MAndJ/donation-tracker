18a1e48eb5dc1812e8daff25cdd7a4c61cb5e52c
Michael Snyder <msnyder1992@gmail.com>
2022-04-20 13:34:35 -0400

    Continuing to iron out issues

--- server/passport.js
+++ server/passport.js
@@ -1,6 +1,9 @@
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const express = require('express');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const db = require('./models');
 
 const router = express.Router();
 
@@ -11,16 +14,62 @@ passport.use(
       clientSecret: "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",
       callbackURL: 'http://localhost:3000/auth/google/callback',
       passReqToCallback: true,
+      store: true
     },
-    function (request, accessToken, refreshToken, profile, done) {
-      //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      //     return done(err, user);
-      //   });
-      console.log(profile);
+    async function (request, accessToken, refreshToken, profile, done) {
+      console.log('profile in strategy: ', profile);
       return done(null, profile);
+    //   const queryString = `
+    //     INSERT INTO users (password, email, chapter_id, first_name, last_name)
+    //     VALUES ($1, $2, $3, $4, $5);
+    //   `;
+    //   const {email, id, given_name: firstName, family_name: lastName} = profile;
+    //   const salt = await bcrypt.genSalt(10);
+    //   const password = await bcrypt.hash(id, salt);
+      
+    //   try {
+    //     const userResult = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+    //     if (!userResult) {
+    //       try {
+    //         const result = await db.query(queryString, [password, email, 3, firstName, lastName]);
+    //         return done(null, profile);
+    //       } catch (error) {
+    //         console.log(error);
+    //         return done(error);
+    //       }
+    //     }
+    //     console.log('user found')
+    //     return done(null, profile);
+    //   } catch (error) {
+    //     console.log(error);
+    //     return done(error);
+    //   }
     }
   )
 );
+
+// async function (request, accessToken, refreshToken, profile, done) {
+//   const queryString = `
+//     INSERT INTO users (password, email, chapter_id, first_name, last_name)
+//     VALUES ($1, $2, $3, $4, $5);
+//   `;
+//   const {email, id, given_name: firstName, family_name: lastName} = profile;
+//   const salt = await bcrypt.genSalt(10);
+//   const password = await bcrypt.hash(id, salt);
+//   request.user = {email: email};
+//   console.log('request.user within passport.js: ', request.user);
+//   // remove the line below when comments are active
+//   // return done(null, profile);
+  
+//   try {
+//     const result = await db.query(queryString, [password, email, 3, firstName, lastName]);
+//     return done(null, profile);
+//   } catch (error) {
+//     console.log(error);
+//     return done(error);
+//   }
+  
+// }
 
 passport.serializeUser((user, done) => {
   done(null, user);
--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -1,26 +1,33 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/login/success', (req, res) => {
-  if (req.user) {
+router.get("/login/success", (req, res) => {
+  if (res.user) {
     return res.status(200).json({
       success: true,
-      message: 'successful',
+      message: "successful",
       user: req.user,
     });
   }
+  return res.send('no user');
 });
 
 router.get(
-  '/google/callback',
-  passport.authenticate('google', {
-    successRedirect: 'http://localhost:8080/chapters',
-    failureRedirect: 'http://localhost:8080/signin',
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "http://localhost:8080/signin",
+    failureMessage: "You have not been authenticated correctly by google",
+    // successRedirect: 'http://localhost:8080/signin'
   }),
-  (req, res) => {
-    return res.send('success');
+  (req, res, next) => {
+    console.log(req.user);
+    // return res.redirect('http://localhost:8080/auth/login/success');
+    return res.json(req.user);
   }
 );
 
