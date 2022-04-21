679eea9d71078a0438731e77cd8d2597e03542e1
Michael Snyder <msnyder1992@gmail.com>
2022-04-20 22:29:14 -0400

    Finished google oAuth cycle. However it is not integrated with state on the front end given the existing auth workflow

--- server/passport.js
+++ server/passport.js
@@ -4,46 +4,67 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const db = require('./models');
+require('dotenv').config();
 
 const router = express.Router();
+
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((email, done) => {
+  done(null, email);
+});
+
+// passport.deserializeUser((email, done) => {
+//   db.query('SELECT * FROM users WHER email = $1', [email])
+//     .then(res => {
+//       done(null, email);
+//     })
+//     .catch(error => done(error));
+// });
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: "590851976751-37cjhbllgiln1e0tnobg22ksn4nepuo2.apps.googleusercontent.com",
-      clientSecret: "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",
-      callbackURL: 'http://localhost:3000/auth/google/callback',
-      passReqToCallback: true,
-      store: true
+      callbackURL: '/auth/google/redirect',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET_ID,
+      // proxy: true,
     },
-    async function (request, accessToken, refreshToken, profile, done) {
-      console.log('profile in strategy: ', profile);
-      return done(null, profile);
-    //   const queryString = `
-    //     INSERT INTO users (password, email, chapter_id, first_name, last_name)
-    //     VALUES ($1, $2, $3, $4, $5);
-    //   `;
-    //   const {email, id, given_name: firstName, family_name: lastName} = profile;
-    //   const salt = await bcrypt.genSalt(10);
-    //   const password = await bcrypt.hash(id, salt);
+    async function (accessToken, refreshToken, profile, done) {
+      console.log(profile);
+
+      const {email, id, given_name: firstName, family_name: lastName} = profile;
+      const findUserQuery = `
+        SELECT * FROM users
+        WHERE email = $1;
+      `;
+      const addUserQuery = `
+        INSERT INTO users (password, email, chapter_id, first_name, last_name)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING email, chapter_id, first_name, last_name;
+      `;
       
-    //   try {
-    //     const userResult = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
-    //     if (!userResult) {
-    //       try {
-    //         const result = await db.query(queryString, [password, email, 3, firstName, lastName]);
-    //         return done(null, profile);
-    //       } catch (error) {
-    //         console.log(error);
-    //         return done(error);
-    //       }
-    //     }
-    //     console.log('user found')
-    //     return done(null, profile);
-    //   } catch (error) {
-    //     console.log(error);
-    //     return done(error);
-    //   }
+      db.query(findUserQuery, [email])
+      .then(async (res) => {
+        if (res.rows[0]) {
+          console.log('existing user: ', res.rows[0]);
+          return done(null, res.rows[0]);
+        } else {
+            const salt = await bcrypt.genSalt(10);
+            const password = await bcrypt.hash(id, salt);
+
+            db.query(addUserQuery, [password, email, 3, firstName, lastName])
+              .then(res => {
+                console.log(res.rows[0]);
+                return done(null, res.rows[0]);
+              })
+              .catch(error => done(error));
+          }
+        })
+        .catch(error => done(error));
     }
   )
 );
@@ -70,13 +91,5 @@ passport.use(
 //   }
   
 // }
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
 
 module.exports = router;
--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -1,34 +1,18 @@
-const router = require("express").Router();
-const passport = require("passport");
+const router = require('express').Router();
+const passport = require('passport');
 
-router.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
 
-router.get("/login/success", (req, res) => {
-  if (res.user) {
-    return res.status(200).json({
-      success: true,
-      message: "successful",
-      user: req.user,
-    });
-  }
-  return res.send('no user');
+router.get('/google/redirect', passport.authenticate('google', {
+  successRedirect: '/dashboard'
+}), (req, res) => {
+  console.log('redirect');
+  const {password, ...publicUserData} = req.user;
+  return res.send(publicUserData);
+  // return res.redirect('http://localhost:8080/signin');
 });
 
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "http://localhost:8080/signin",
-    failureMessage: "You have not been authenticated correctly by google",
-    // successRedirect: 'http://localhost:8080/signin'
-  }),
-  (req, res, next) => {
-    console.log(req.user);
-    // return res.redirect('http://localhost:8080/auth/login/success');
-    return res.json(req.user);
-  }
-);
 
 module.exports = router;
